e41e3c17259db5a386b100e85aa33cf0f5c0cbdf
LeeJaeJun1 <jj030207@naver.com>
2025-01-10 20:53:15 +0900

    2week

---
+++ 실습/week2/week2_2.java
@@ -0,0 +1,15 @@
+package week2;
+
+public class week2_2 {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        byte b2 = 20;
+        int bSum = b1 + b2;
+        System.out.println("bSum의 값: " + bSum);
+        char c1 = 'A';
+        char c2 = 1;
+        int cSum = c1 + c2;
+        System.out.println("cSum의 값: " + cSum);
+        System.out.print("cSum의 문자: " + (char)cSum);
+    }
+}
